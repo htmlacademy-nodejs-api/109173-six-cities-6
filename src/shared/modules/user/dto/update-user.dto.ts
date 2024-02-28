4445439fbf44544379d7208f796a327a95b150ad
Dmitriy Nosikov <deimon1993rf2@yandex.ru>
2024-02-28 14:48:33 +0300

    Изменена валидация UpdateUser.dto

--- src/shared/modules/user/dto/update-user.dto.ts
+++ src/shared/modules/user/dto/update-user.dto.ts
@@ -1,9 +1,46 @@
 // Интерфейс (контракт) передачи данных в сервис для дальнейшей работы с сущностью User
-export class UpdateUserDTO {
-  public name?: string;
-  public email?: string;
-  public avatarUrl?: string;
-  public password?: string;
-  public isPro?: boolean;
-  public token?: string;
+import {
+  IsAlphanumeric,
+  IsArray,
+  IsBoolean,
+  IsEmail,
+  IsMongoId,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength
+} from 'class-validator';
+import { User } from '../../../types/user.type.js';
+import { UserProps } from '../user.constant.js';
+import { UserErrorText } from './create-user.messages.js';
+
+export class CreateUserDTO implements User {
+  @MaxLength(UserProps.name.MAX_LENGTH, { message: UserErrorText.name.MAX })
+  @MinLength(UserProps.name.MIN_LENGTH, { message: UserErrorText.name.MIN })
+  @IsString()
+  @IsOptional()
+  public name!: string;
+
+  @IsEmail()
+  @IsOptional()
+  public email!: string;
+
+  @IsString()
+  @IsOptional()
+  public avatarUrl!: string;
+
+  @MaxLength(UserProps.password.MAX_LENGTH, { message: UserErrorText.password.MAX })
+  @MinLength(UserProps.password.MIN_LENGTH, { message: UserErrorText.password.MIN })
+  @IsAlphanumeric()
+  @IsOptional()
+  public password!: string;
+
+  @IsBoolean()
+  @IsOptional()
+  public isPro!: boolean;
+
+  @IsMongoId({ each: true })
+  @IsArray()
+  @IsOptional()
+  public favoriteOffers!: string[];
 }
